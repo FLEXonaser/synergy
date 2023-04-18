a14ad54a1163ab315d352b03ade05456b447b0b9
FLEXonase <FLEXstudio-mc@yandex.ru>
2023-04-18 20:38:45 +0300

    first commit

---
+++ M2DZ5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <limits>
+
+using namespace std;
+template<class T1, class T2>
+class Info {
+public:
+	T1 i;
+	Info(T1 i) {
+		this->i = i;
+	}
+
+	void init() {
+
+		cout << i << " Type: " << typeid(i).name() << " Size : " << sizeof(i) << endl;
+		cout << "Range: " << numeric_limits<T1>::min() << ", " << numeric_limits<T1>::max() << endl;
+		// Нашёл в интернете интересную библиотеку, благодаря которой очень просто можно вывести минимальные и максимальные диапазоны переменных.
+		// Надеюсь, так можно:)
+	}
+	
+};
+
+int main()
+{
+
+	Info<int, string> g(10);
+	g.init();
+
+}
